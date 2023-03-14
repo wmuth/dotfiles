322d641bd5599fdb9afe01a014031817ba01bb7e
wmuth <wmut@protonmail.com>
2023-03-14 20:42:54 +0100

    add alacritty

--- install-em.sh
+++ install-em.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+# ../ is home, avoided ~ intentionally
+
+# alacritty
+mkdir -p ../.config/alacritty/
+cp ./alacritty.yml ../.config/alacritty/alacritty.yml
+
+# lazygit
+
+# nvim
+
+# tmux
+
+# vscodum
+
+# zsh
