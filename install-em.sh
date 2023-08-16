e4752524e87f7912448578a2fb9778befc2315b4
wmuth <wmut@protonmail.com>
2023-08-16 14:39:19 +0200

    Update

--- install-em.sh
+++ install-em.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 # required due to mostly executing through ansible
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 # alacritty
 mkdir -p ~/.config/alacritty/
 cp $SCRIPT_DIR/alacritty.yml ~/.config/alacritty/alacritty.yml
 
-# lazygit
-
 # nvim
+mkdir -p ~/.config/nvim/
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
 
-# tmux
+# tlp
+cp $SCRIPT_DIR/tlp.conf ~/tlp.conf
 
-# vscodum
-
-# zsh
+# zsh, omz
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sed -ie 's/plugins=(git)/plugins=(\n    git\n    zsh-autosuggestions\n    zsh-syntax-highlighting\n        )/g' ~.zshrc
+cat $SCRIPT_DIR/alias.txt >> (~.zshrc)
