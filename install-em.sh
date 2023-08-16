e77156e74fe42b2342d2a55cc8ac8e908b9561a3
wmuth <wmut@protonmail.com>
2023-08-16 15:01:57 +0200

    Fixes

--- install-em.sh
+++ install-em.sh
@@ -8,13 +8,14 @@ cp $SCRIPT_DIR/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 # nvim
 mkdir -p ~/.config/nvim/
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
+#rm -rf ~/.config/nvim ~/.local/share/nvim ~/.local/state/nvim ~/.cache/nvim
+#git clone https://github.com/LazyVim/starter ~/.config/nvim
+#rm -rf ~/.config/nvim/.git
 
 # tlp
 cp $SCRIPT_DIR/tlp.conf ~/tlp.conf
 
 # zsh, omz
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-sed -ie 's/plugins=(git)/plugins=(\n    git\n    zsh-autosuggestions\n    zsh-syntax-highlighting\n        )/g' ~.zshrc
-cat $SCRIPT_DIR/alias.txt >> (~.zshrc)
+sed -ie 's/plugins=(git)/plugins=(\n      git\n    zsh-autosuggestions\n    zsh-syntax-highlighting\n    )/g' ~/.zshrc
+cat $SCRIPT_DIR/zshrc.txt >>~/.zshrc
